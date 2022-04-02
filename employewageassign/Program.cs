a7a48437593d3b678f81e08ad789516f4871a03d
Thiru <vijayofficial580@gmail.com>
2022-04-02 10:07:32 +0530

    first commit

---
+++ employewageassign/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace employewageassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if (isPresent == empInput)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee Absent");
+        }
+    }
+}
