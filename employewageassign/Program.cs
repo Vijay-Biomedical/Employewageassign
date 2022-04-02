00a63ff9d39f424f0ccf7b04551fc62e6748045c
Thiru <vijayofficial580@gmail.com>
2022-04-02 10:22:52 +0530

    uc2

--- employewageassign/Program.cs
+++ employewageassign/Program.cs
@@ -8,13 +8,23 @@ namespace employewageassign
     {
         static void Main(string[] args)
         {
-            int isPresent = 1;
+            int isPresent = 1, EMPWAGE_WAGE_PER_HR = 20, EMP_HR = 0, EMP_WAGE = 0;
             Random random = new Random();
             int empInput = random.Next(0, 2);
             if (isPresent == empInput)
+            {
                 Console.WriteLine("Employee is Present");
+                EMP_HR = 8;
+            }
             else
+            {
                 Console.WriteLine("Employee Absent");
+                EMP_HR = 0;
+            }
+
+            EMP_WAGE = EMP_HR * EMPWAGE_WAGE_PER_HR;
+            Console.WriteLine("DAILY WAGE OF EMPLOYE:" + EMP_WAGE);
+            Console.ReadLine();
         }
     }
 }
