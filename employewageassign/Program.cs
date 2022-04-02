2c00ff0cff6c459b7ac3fe4c1fde148222c4a78a
Thiru <vijayofficial580@gmail.com>
2022-04-02 10:57:15 +0530

    uc3

--- employewageassign/Program.cs
+++ employewageassign/Program.cs
@@ -8,22 +8,30 @@ namespace employewageassign
     {
         static void Main(string[] args)
         {
-            int isPresent = 1, EMPWAGE_WAGE_PER_HR = 20, EMP_HR = 0, EMP_WAGE = 0;
+            int EMPWAGE_WAGE_PER_HR = 20, EMP_HR = 0, EMP_WAGE = 0;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             Random random = new Random();
-            int empInput = random.Next(0, 2);
-            if (isPresent == empInput)
+            int empInput = random.Next(0, 3);
+            switch(empInput)
             {
-                Console.WriteLine("Employee is Present");
+              case FULL_TIME:
+                Console.WriteLine(" Fulltime Employee is Present");
                 EMP_HR = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee Absent");
+                 break;
+              case PART_TIME:
+                Console.WriteLine(" Parttime Employee is Present");
+                Console.WriteLine("");
+                EMP_HR = 4;
+                break;
+              default:
+                Console.WriteLine("Employee aAbsent");
                 EMP_HR = 0;
+                break;
             }
 
             EMP_WAGE = EMP_HR * EMPWAGE_WAGE_PER_HR;
-            Console.WriteLine("DAILY WAGE OF EMPLOYE:" + EMP_WAGE);
+            Console.WriteLine("DAILY WAGE OF EMPLOYES:" + EMP_WAGE);
             Console.ReadLine();
         }
     }
